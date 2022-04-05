ba245e560d8b559e6acfb8cdf12e64f2f7be7bbc
choudhary-github <kumarankit.ak325@gmail.com>
2022-04-06 02:42:20 +0530

    closure

--- JavaScript/Closure/script.js
+++ JavaScript/Closure/script.js
@@ -1,13 +1,16 @@
 function outer(arg1){
     let var1 = 10
-    var x = 50
-    function inner(arg2){
-        let var2 = 20
-        let x = 500
-        console.log(arg1,var1,arg2,var2,x)
-    }
-    return inner
+        function middle(arg2){
+            let var2 = 51
+            function inner(arg3){
+                let var3 = 20
+                console.log(arg1,var1,arg2,var2,arg3,var3)
+            }
+            return inner
+        }
+        return middle
 }
 
 let x = outer("param1")
-x("param2")
+let y = x("param2")
+y('param3')
