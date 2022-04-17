c157d5a9ac31f5f1effcca081b1fef7bdc1e5965
choudhary-github <kumarankit.ak325@gmail.com>
2022-04-17 12:24:20 +0530

    game

--- Assignment/2d Collider Game/script.js
+++ Assignment/2d Collider Game/script.js
@@ -21,12 +21,12 @@ class Player extends Box{
     }
     move(){
         this.x += this.speed
-        if(this.x + this.size > 500){
-            this.speed = -(Math.abs(this.speed))
-        }
-        if(this.x == 0){
-            this.speed = Math.abs(this.speed)
-        }
+        // if(this.x + this.size > 500){
+        //     this.speed = -(Math.abs(this.speed))
+        // }
+        // if(this.x == 0){
+        //     this.speed = Math.abs(this.speed)
+        // }
     }
 }
 class Enemy extends Box{
@@ -64,12 +64,34 @@ function drawBox(box){
     context.fillStyle = box.color
     context.fillRect(box.x,box.y,box.size,box.size)
 }
+
+function colliding(){if (
+    player.x + player.size >= e1.x &&
+    player.x <= e1.x + e1.size && 
+    player.y + player.size >= e1.y && 
+    player.y <= e1.y + e1.size || 
+    player.x + player.size >= e2.x &&
+    player.x <= e2.x + e2.size && 
+    player.y + player.size >= e2.y && 
+    player.y <= e2.y + e2.size ||
+    player.x + player.size >= e3.x &&
+    player.x <= e3.x + e3.size && 
+    player.y + player.size >= e3.y && 
+    player.y <= e3.y + e3.size
+)
+{
+    console.log('colliding')
+    gameon = false
+}
+}
+
 function updateGame(){
-    if(!gameon) return
+    if(!gameon) return(window.alert('Game Over : \nPlease restart the Page'))
     context.clearRect(0,0,500,500)
     e1.move()
     e2.move()
     e3.move()
+    colliding()
     player.move()
     drawBox(e1)
     drawBox(e2)
