ebfba641c122aee0397c688cca97d3e551f2a9ec
choudhary-github <kumarankit.ak325@gmail.com>
2022-04-25 23:21:30 +0530

    fizzbuzz

--- fizzbuzz/better way/script.js
+++ fizzbuzz/better way/script.js
@@ -9,11 +9,19 @@ window.onload = () =>{
         start = Date.now()
         list.innerHTML = ''
         let n = parseInt(input.value)
+
+        //let c3 = 0
+        //let c5 = 0
+
         for( i = 1; i <= n; i++){
+
+            // c3 ++
+            // c5 ++
             let li = document.createElement('li')
             let print = ''
-            if (i % 3 == 0) print += 'fizz'
-            if (i % 5 == 0) print += 'buzz'
+
+            if (i % 3 == 0)/* if(c++ = 3) {c3 = 0; print += fizz} */ print += 'fizz'
+            if (i % 5 == 0)/* if(c++ = 5) {c5 = 0; print += buzz} */ print += 'buzz'
             if (print === '') print = i
 
             li.textContent = print
