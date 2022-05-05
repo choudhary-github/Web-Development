862a658d78aa2081ac604f3baf834b3b0fbbbaad
choudhary-github <kumarankit.ak325@gmail.com>
2022-05-05 22:46:25 +0530

    start and stop button function

--- JavaScript/delay/script.js
+++ JavaScript/delay/script.js
@@ -2,14 +2,20 @@ let btnWait = document.getElementById('btnWait')
 let btnCount = document.getElementById('btnCount')
 let divStatus = document.getElementById('divStatus')
 let divValue = document.getElementById('divValue')
+let btnStop = document.getElementById('btnStop')
+
 
 btnWait.onclick = function(){
     divStatus.textContent = 'Waiting'
     console.log('waiting')
-    setTimeout(() => {
+    let brk = setTimeout(() => {
         divStatus.textContent = 'Done'
     },5000)
-    
+    btnStop.onclick = (()=>{
+        clearTimeout(brk)
+        divStatus.textContent = 'Stopped'
+        console.log('Stopped')
+    })
 }
 
 let count = 0
