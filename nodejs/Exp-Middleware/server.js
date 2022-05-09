56e7fd5e46522081837281269c0fa1040e949e2b
choudhary-github <kumarankit.ak325@gmail.com>
2022-05-10 02:42:14 +0530

    server.js

--- nodejs/Exp-Middleware/server.js
+++ nodejs/Exp-Middleware/server.js
@@ -1,25 +1,3 @@
 const express = require('express')
 
 const app = express()
-function m1 (req,res,next){
-    console.log('pre m1');
-    next()
-    console.log('post m1');
-}function m2 (req,res,next){
-    console.log('pre m2');
-    next()
-    console.log('post m2');
-}function m3 (req,res,next){
-    console.log('pre m3');
-    next()
-    console.log('post m3');
-}
-app.get('/hello',m1,m2,m3,(req,res)=>{
-    console.log('pre - send')
-    res.send('Hello World')
-    console.log('post - send');
-})
-
-app.listen('4343',()=>{
-    console.log('Server Started on http://localhost:4343')
-})
